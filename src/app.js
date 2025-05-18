c6ef9dd6c3e1baf538618186e62618a9f52e3624
Juanje1997 <juanjesusdp1@outlook.es>
2025-05-18 21:13:05 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function aleatorizar(){
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+let whoAleatorio= who[Math.floor(Math.random()*who.length)];
+let actionAleatorio= action[Math.floor(Math.random()*action.length)];
+let whatAleatorio= what[Math.floor(Math.random()*what.length)];
+let whenAleatorio= when[Math.floor(Math.random()*when.length)];
+
+const excusa= `${whoAleatorio} ${actionAleatorio} ${whatAleatorio} ${whenAleatorio}.`;
+
+return excusa;
+
+}
+
+const elemento = document.getElementById("excuse");
+const excusa = aleatorizar();
+elemento.innerHTML += "<br><strong>" + excusa +"<strong/>";
+
 };
+
+
